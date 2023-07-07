97cf978573366cf645637f8f3921f2f411d94404
Mark Kamuda <mark.kamuda@gmail.com>
2023-07-06 22:11:49 -0400

    first pass at fine tuning

--- local_llm.py
+++ local_llm.py
@@ -1,24 +1,67 @@
-from langchain import PromptTemplate, LLMChain
-from transformers import AutoModelForCausalLM, pipeline
+from langchain import LLMChain
+from transformers import pipeline
 from langchain import HuggingFacePipeline
+import asyncio
+import functools
+import typing
+import os
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.prompts.prompt import PromptTemplate
+
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
 
 
-def llm(input_prompt: str) -> str:
-    template = """Question: {question}
+def read_all_few_shot_examples(file_path: str):
+    file_names = os.listdir(file_path)
+    all_examples = [read_text_file(os.path.join(file_path, file)) for file in file_names if file.endswith('.txt')]
+    return all_examples
 
-    Answer: Let's think step by step."""
 
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    print(prompt)
+def to_thread(func: typing.Callable) -> typing.Coroutine:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.to_thread(func, *args, **kwargs)
 
-    # max_length has typically been deprecated for max_new_tokens
-    pipe = pipeline(
-        "text-generation", model='gpt-neo-1.3B', max_new_tokens=64, model_kwargs={"temperature": 0}
-    )
-    hf = HuggingFacePipeline(pipeline=pipe)
+    return wrapper
 
-    llm_chain = LLMChain(prompt=prompt, llm=hf)
 
-    output = llm_chain.run(input_prompt)
+class LocalLLM:
+    def __init__(self, model_name='gpt-neo-1.3B', temperature=0):
+        self.llm_chain = self.init_llm_chain(model_name, temperature)
 
-    return output
+    @to_thread
+    def local_llm(self, input_prompt: str) -> str:
+        output = self.llm_chain.run(input_prompt)
+        return output
+
+    @staticmethod
+    def init_llm_chain(model='gpt-neo-1.3B', temperature=0):
+        all_few_shot_examples = read_all_few_shot_examples(file_path=r"D:\LLMDiscord\text_data\recipes")[:6]
+
+        main_question = 'Give me a dinner menu with a grocery list for the following few days.'
+
+        all_questions = [main_question] * len(all_few_shot_examples)
+        all_answers = all_few_shot_examples
+
+        examples = [{"question": question, "answer": answer} for question, answer in zip(all_questions, all_answers)]
+        example_prompt = PromptTemplate(input_variables=["question", "answer"],
+                                        template="Question: {question}\n{answer}")
+
+        prompt = FewShotPromptTemplate(
+            examples=examples,
+            example_prompt=example_prompt,
+            suffix="Question: {input}",
+            input_variables=["input"])
+
+        pipe = pipeline(
+            "text-generation", model=model, max_new_tokens=128, model_kwargs={"temperature": temperature}
+        )
+
+        hf = HuggingFacePipeline(pipeline=pipe)
+
+        llm_chain = LLMChain(prompt=prompt, llm=hf)
+        return llm_chain
+
+
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import discord
-import asyncio
-import functools
-import typing
 import os
 from dotenv import load_dotenv
-from langchain import PromptTemplate, LLMChain
-from transformers import pipeline
-from langchain import HuggingFacePipeline
+from local_llm import LocalLLM
 
 load_dotenv()
 
@@ -15,37 +10,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+local_llm_model = LocalLLM()
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-
-def to_thread(func: typing.Callable) -> typing.Coroutine:
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        return await asyncio.to_thread(func, *args, **kwargs)
-    return wrapper
-
-
-@to_thread
-def local_llm(input_prompt: str) -> str:
-    template = """Question: {question}
-
-    Answer: Let's think step by step."""
-
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    print(prompt)
-
-    # max_length has typically been deprecated for max_new_tokens
-    pipe = pipeline("text-generation", model='gpt-neo-1.3B', max_new_tokens=64, model_kwargs={"temperature": 0})
-    hf = HuggingFacePipeline(pipeline=pipe)
-
-    llm_chain = LLMChain(prompt=prompt, llm=hf)
-
-    output = llm_chain.run(input_prompt)
-
-    return output
 
 
 @client.event
@@ -53,12 +22,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('/LocalLLM'):
-
+    if message.content.startswith('RecipeMe'):
         prompt = message.content[9:]
         if len(prompt) == 0:
             message.channel.send('What can I help you with?')
-        llm_output = await local_llm(prompt)
+        llm_output = await local_llm_model.local_llm(prompt)
 
         await message.channel.send(llm_output)
 
